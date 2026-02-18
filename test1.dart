9b0458250bc90338137b5e9e206db7787b234667
Imran_Mahmud <mahmudulimran396@gmail.com>
2026-02-18 15:34:49 +0600

    Add files via upload

---
+++ test1.dart
@@ -0,0 +1,18 @@
+void main(){
+
+for(int i =1; i <=50; i++){
+ if (i% 2== 0){
+  print("1 to 50 even number is $i");
+ }
+}
+
+for(int i = 2; i <=50; i+=2 ){
+  print(i);
+}
+int i = 0;
+while( i <=50){
+  print(i);
+  i+=2;
+}
+  print("Hello World 1234");
+}
